fd14a14ec7cd89ef4d5b800073d27381884e10a8
Morronel <zahar.kogut@gmail.com>
2018-10-25 02:28:24 +0300

    ex 88.2

--- src/com/morronel/Main.java
+++ src/com/morronel/Main.java
@@ -3,12 +3,30 @@ package com.morronel;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Student pekka = new Student("Pekka Mikkola", "013141590");
-        System.out.println("name: " + pekka.getName());
-        System.out.println("studentnumber: " + pekka.getStudentNumber());
-        System.out.println(pekka);
+        ArrayList<Student> list = new ArrayList<Student>();
+        Scanner scanner = new Scanner(System.in);
+        String newName;
+        String newNumber;
+        int i = 0;
+
+        System.out.println("name: ");
+        newName = scanner.nextLine();
+
+        while (!newName.equals("")) {
+            System.out.println("number: ");
+            newNumber = scanner.nextLine();
+            list.add(new Student(newName, newNumber));
+            i++;
+            System.out.println("name: ");
+            newName = scanner.nextLine();
+        }
+
+        for (int j = 0; j < i; j++) {
+            System.out.println(list.get(j).getName() + " (" + list.get(j).getStudentNumber() + ")");
+        }
     }
 }
