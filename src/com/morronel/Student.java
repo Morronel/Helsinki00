71b29f4f68fef9fb4c75eddce35cdc468f9fcdf6
Morronel <zahar.kogut@gmail.com>
2018-10-25 04:32:51 +0300

    ex 88.3 fx

--- src/com/morronel/Student.java
+++ src/com/morronel/Student.java
@@ -4,17 +4,17 @@ public class Student {
     private String name;
     private String studentNumber;
 
+    public Student(String newName, String newStudentNumber) {
+        name = newName;
+        studentNumber = newStudentNumber;
+    }
+
     public String getName() {
         return name;
     }
 
     public String getStudentNumber() {
         return studentNumber;
-    }
-
-    public Student(String newName, String newStudentNumber) {
-        name = newName;
-        studentNumber = newStudentNumber;
     }
 
     @Override
